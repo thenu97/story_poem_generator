b06ad2eda9915db7417a08b17c06dbeff1f31d07
thenu97 <thenuja_1@outlook.com>
2020-04-09 22:56:44 +0000

    path defined

--- scripts/run_after.sh
+++ scripts/run_after.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 source /var/lib/jenkins/.bashrc
 
-python3 -m coverage run --source=. -m pytest scripts/tests/testing.py
+python3 -m coverage run --source=. -m pytest ~/Service_1/tests/testing.py
 python3 -m coverage report -m
